57c6d5e2ee5975efcd699e9ecc6777848792dcb0
jonathan <jonathan876200@gmail.com>
2017-11-07 11:01:41 +0800

    OnClick

--- app/src/main/java/tom/com/bmi6/MainActivity.java
+++ app/src/main/java/tom/com/bmi6/MainActivity.java
@@ -1,18 +1,32 @@
 package tom.com.bmi6;
 
+import android.content.res.Resources;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    String s = new String("abc");
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
 
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bHelp = (Button) findViewById(R.id.b_help);
+         bHelp.setOnClickListener(listener);
+
+        getResources().getString(R.string.app_name);
+
     }
+
     public void bmi (View view){
         EditText weight = (EditText)findViewById(R.id.ed_weight);
         EditText height = (EditText)findViewById(R.id.ed_height);
@@ -20,8 +34,9 @@ public class MainActivity extends AppCompatActivity {
         float h = Float.parseFloat(height.getText().toString());
         float bmi = w/(h*h);
         new AlertDialog.Builder(this)
-                .setMessage("Your bmi is" +bmi)
-                .setPositiveButton("OK", null)
+                .setMessage(getString(R.string.your_bmi_is) + bmi)
+                .setTitle(R.string.my_title)
+                .setPositiveButton(R.string.ok, null)
                 .show();
     }
 }
